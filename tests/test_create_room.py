69335c47c55e3fd1125205cdc1307ecbd3755f94
nora-weisser <eleo.belova@yandex.ru>
2025-08-22 16:24:00 +0200

    feat: add one more assertion to verify if the post request was successful

--- tests/test_create_room.py
+++ tests/test_create_room.py
@@ -1,11 +1,13 @@
 from utils import api_util
-from utils.assertions import assert_status_code
+from utils.assertions import assert_status_code, assert_room_name_exists
 from TestData.expected_responses import success_response, authentication_required_response
 
 def test_create_new_room_returns_200(auth_headers, room_url, new_room_data):
     response = api_util.post_api(room_url, new_room_data, auth_headers)
     assert_status_code(response, 200)
     assert response.json() == success_response
+    rooms_list = api_util.get_api(room_url)
+    assert_room_name_exists(rooms_list.json(), new_room_data["roomName"])
 
 
 def test_create_new_room_without_authentication_returns_401(room_url, new_room_data):
--- utils/assertions.py
+++ utils/assertions.py
@@ -7,3 +7,7 @@ def assert_status_code(response, expected_status: int):
         f"Expected {expected_status} but got {actual_status}. "
         f"Response body: {getattr(response, 'text', '')}"
 )
+
+@allure.step('Asserts that newly created room is present in the list of rooms')
+def assert_room_name_exists(rooms_list, room_name):
+    assert any(room["roomName"] == room_name for room in rooms_list["rooms"]), f"Room name '{room_name}' not found!"
